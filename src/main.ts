2f1df1004dfb51b279b12a4a5732c53bef155052
eunkangchoi <ekk12mv2@gmail.com>
2022-11-07 22:14:07 +0900

    :children_crossing: feat: Custom Exception Filter 적용
    all-exceptions.filter 생성 및 전역 적용

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { AllExceptionsFilter } from './all-exceptions/all-exceptions.filter';
+import { AllExceptionsFilter } from './filters/all-exceptions/all-exceptions.filter';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './http-exception/http-exception.filter';
+import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalPipes(new ValidationPipe());
+
+  // HttpExceptionFilter을 전역 스코프에 적용.
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
