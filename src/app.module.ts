75fe2480e9b8cf32d22b0b183a97cece0748f857
eunkangchoi <ekk12mv2@gmail.com>
2022-11-08 04:03:50 +0900

    :loud_sound: feat: Winston Logger 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,32 @@ import { UsersModule } from './users/users.module';
 import { MarketsModule } from './markets/markets.module';
 import { ProductsModule } from './products/products.module';
 
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+
 @Module({
-  imports: [UsersModule, MarketsModule, ProductsModule],
+  imports: [
+    UsersModule,
+    MarketsModule,
+    ProductsModule,
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              prettyPrint: true,
+              colors: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
