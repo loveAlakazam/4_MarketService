70c59ad64232fa6096249aa4e47cc0fd52274d28
eunkangchoi <ekk12mv2@gmail.com>
2022-11-08 21:08:02 +0900

    :wrench: chore: MongooseModule 과 서비스 연결

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schemas/product.schema';
 
 @Module({
+  imports: [
+    // forFeature() : 환경설정역할 -> 모델을 정의
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    // MongooseModule.forFeature(....) // Products외 다른 콜렉션을 사용할 때 추가하면된다.
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
 export class ProductsModule {}
