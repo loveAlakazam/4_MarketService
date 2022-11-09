3ad953ea2aa38a67a6dadf04ddae2dce929d28c9
eunkangchoi <ekk12mv2@gmail.com>
2022-11-09 15:16:57 +0900

    :heavy_plus_sign: chore: 의존성 추가

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,6 +4,8 @@ import { ProductsController } from './products.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './schemas/product.schema';
 import { ProductsRepository } from './products.repository';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/commons/filters/http-exception/http-exception.filter';
 
 @Module({
   imports: [
@@ -11,6 +13,10 @@ import { ProductsRepository } from './products.repository';
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
   controllers: [ProductsController],
-  providers: [ProductsService, ProductsRepository],
+  providers: [
+    ProductsService,
+    ProductsRepository,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class ProductsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule,
+  ],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersRepository],
 })
 export class UsersModule {}
