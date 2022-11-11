9c8a6fc0a66d6131219ba68ca54710d97473010c
eunkangchoi <ekk12mv2@gmail.com>
2022-11-12 01:57:29 +0900

    test: product 유닛테스트 환경 구축

--- src/products/test/products.service.spec.ts
+++ src/products/test/products.service.spec.ts
@@ -1,18 +1,33 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ProductsRepository } from '../products.repository';
 import { ProductsService } from '../products.service';
+import { Product } from '../schemas/product.schema';
 
 describe('ProductsService', () => {
-  let service: ProductsService;
+  let productService: ProductsService;
+  let productRepository: ProductsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        ProductsRepository,
+        {
+          provide: getModelToken(Product.name),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          useFactory: () => {},
+        },
+      ],
     }).compile();
 
-    service = module.get<ProductsService>(ProductsService);
+    productService = await module.get<ProductsService>(ProductsService);
+    productRepository = await module.get<ProductsRepository>(
+      ProductsRepository,
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(productService).toBeDefined();
   });
 });
