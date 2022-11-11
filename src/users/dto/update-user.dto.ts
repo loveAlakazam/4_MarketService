fbfe951d77fbd9d79f3c189e250984a334f81004
eunkangchoi <ekk12mv2@gmail.com>
2022-11-12 01:27:24 +0900

    fix: sellerNickname 필드 추가

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto {
   isSeller?: boolean;
+  sellerNickname?: string;
   phoneNumber?: string;
   name?: string;
   password?: string;
