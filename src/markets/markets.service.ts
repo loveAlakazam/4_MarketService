d54c46ea4da0bb07714eb0ce49e64d26cdae1503
eunkangchoi <ekk12mv2@gmail.com>
2022-11-12 01:57:05 +0900

    fix: market.service 생성자 수정 - productRepository 추가

--- src/markets/markets.service.ts
+++ src/markets/markets.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsRepository } from '../products/products.repository';
 import { CreateMarketDto } from './dto/create-market.dto';
 import { UpdateMarketDto } from './dto/update-market.dto';
 import { MarketsRepository } from './markets.repository';
 
 @Injectable()
 export class MarketsService {
-  constructor(private readonly repository: MarketsRepository) {}
+  constructor(
+    private readonly repository: MarketsRepository,
+    private readonly productRepository: ProductsRepository,
+  ) {}
   create(createMarketDto: CreateMarketDto) {
     return 'This action adds a new market';
   }
