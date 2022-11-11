78da173e6117079dae5beb1206417f3c3b1d334d
eunkangchoi <ekk12mv2@gmail.com>
2022-11-11 16:49:06 +0900

    feat: LocalStrategy (세션) 추가

---
+++ src/auth/strategies/auth-local.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(private readonly authService: AuthService) {
+    super({ usernameField: 'email', passwordField: 'password' });
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('아이디와 비밀번호가 일치하지 않습니다.');
+    }
+    return user;
+  }
+}
