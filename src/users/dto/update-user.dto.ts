c2c816ea683cbc2ee7529289b8fb49934a753c21
eunkangchoi <ekk12mv2@gmail.com>
2022-11-11 19:52:54 +0900

    feat: 유저정보 수정 dto 추가

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  isSeller?: boolean;
+  phoneNumber?: string;
+  name?: string;
+  password?: string;
+}
