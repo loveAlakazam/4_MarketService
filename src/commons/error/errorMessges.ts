f59c8f65ee2494799a56cc51773eb0abf54590ae
eunkangchoi <ekk12mv2@gmail.com>
2022-11-11 17:11:56 +0900

    init: 커스텀 에러문구 생성

---
+++ src/commons/error/errorMessges.ts
@@ -0,0 +1,22 @@
+export const Err = {
+  USER: {
+    NOT_FOUND: {
+      code: 404,
+      message: '존재하지 않는 사용자 입니다.',
+    },
+    EXIST_USER: {
+      code: 400,
+      message: '이미 존재하는 사용자 입니다.',
+    },
+    EXIST_SELLER: {
+      code: 400,
+      message: '이미 존재하는 셀러 입니다.',
+    },
+  },
+  PRODUCT: {
+    NOT_FOUND: {
+      code: 404,
+      message: '존재하지 않는 상품 입니다.',
+    },
+  },
+};
