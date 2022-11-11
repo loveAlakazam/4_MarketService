2bdec0fe0549904245f4917ab172f51d3934f70b
eunkangchoi <ekk12mv2@gmail.com>
2022-11-12 01:57:20 +0900

    test: 마켓 유닛테스트 환경구축

--- src/markets/test/markets.controller.spec.ts
+++ src/markets/test/markets.controller.spec.ts
@@ -1,17 +1,40 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MarketsController } from '../markets.controller';
+import { MarketsRepository } from '../markets.repository';
 import { MarketsService } from '../markets.service';
+import { Market } from '../schemas/markets.schema';
+import { Product } from '../../products/schemas/product.schema';
+import { ProductsRepository } from '../../products/products.repository';
 
 describe('MarketsController', () => {
   let controller: MarketsController;
+  let service: MarketsService;
+  let repository: MarketsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MarketsController],
-      providers: [MarketsService],
+      providers: [
+        MarketsService,
+        MarketsRepository,
+        ProductsRepository,
+        {
+          provide: getModelToken(Market.name),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Product.name),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          useFactory: () => {},
+        },
+      ],
     }).compile();
 
     controller = module.get<MarketsController>(MarketsController);
+    service = module.get<MarketsService>(MarketsService);
+    repository = module.get<MarketsRepository>(MarketsRepository);
   });
 
   it('should be defined', () => {
--- src/markets/test/markets.service.spec.ts
+++ src/markets/test/markets.service.spec.ts
@@ -1,15 +1,36 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MarketsService } from '../markets.service';
+import { MarketsRepository } from '../markets.repository';
+import { ProductsRepository } from '../../products/products.repository';
+import { Market } from '../schemas/markets.schema';
+import { Product } from '../../products/schemas/product.schema';
 
 describe('MarketsService', () => {
   let service: MarketsService;
+  let repository: MarketsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MarketsService],
+      providers: [
+        MarketsService,
+        MarketsRepository,
+        ProductsRepository,
+        {
+          provide: getModelToken(Market.name),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Product.name),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          useFactory: () => {},
+        },
+      ],
     }).compile();
 
     service = module.get<MarketsService>(MarketsService);
+    repository = module.get<MarketsRepository>(MarketsRepository);
   });
 
   it('should be defined', () => {
