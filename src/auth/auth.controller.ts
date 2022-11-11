1d9da9dfd26b23b868e2266119ec02ec6da5a6bf
eunkangchoi <ekk12mv2@gmail.com>
2022-11-11 16:49:56 +0900

    feat: 유저 컨트롤러 추가 (세션적용)

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Response,
+  UseFilters,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/commons/filters/http-exception/http-exception.filter';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { AuthenticatedGuard, LocalAuthGuard } from './guards/local-auth.guard';
+import { User } from './decorators/auth.decorator';
+
+@UseFilters(HttpExceptionFilter)
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  /**
+   * @url [POST] /api/auth/sign-up
+   * @description 회원가입
+   */
+  @Post('sign-up')
+  async signUp(@Body() signUpDto: SignUpDto) {
+    await this.authService.signUp(signUpDto);
+  }
+
+  /**
+   * @url [POST] /api/auth/sign-in
+   * @description: 로그인
+   */
+  @UseGuards(LocalAuthGuard)
+  @Post('sign-in')
+  async signIn(@User() user): Promise<void> {}
+
+  /**
+   * @url [GET] /api/auth/sign-out
+   * @description: 로그아웃
+   */
+  //   @UseGuards(AuthenticatedGuard)
+  @Get('sign-out')
+  async signOut() {}
+}
