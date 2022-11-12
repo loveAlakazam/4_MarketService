5059b4e784e87964c6c2f53b536c8d59ae4c3774
eunkangchoi <ekk12mv2@gmail.com>
2022-11-13 02:21:07 +0900

    :sparkles: feat: product enum 추가 (국가,카테고리)

---
+++ src/products/enums/categories.ts
@@ -0,0 +1,15 @@
+/**
+ * 상품의 카테고리를 나타냅니다.
+ * - 기본값은 NO 로 설정합니다.
+ */
+export enum PRODUCT_CATEGORIES {
+  NO = '카테고리 없음',
+  ETC = '기타',
+  CLOTHS = '의류',
+  BAGS = '가방',
+  ELECTRONICS = '전자제품',
+  GROCERIES = '음식',
+  FURNITURES = '가구',
+  BEAUTIES = '화장품',
+  INTERIOR = '인테리어',
+}
