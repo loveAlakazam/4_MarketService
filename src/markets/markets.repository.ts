e204ac13b7550293bd9ae13ab06eb612a0d4b645
eunkangchoi <ekk12mv2@gmail.com>
2022-11-13 02:35:42 +0900

    fix: 유저 다큐먼트 생성자 주입 추가

--- src/markets/markets.repository.ts
+++ src/markets/markets.repository.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Market, MarketDocument } from './schemas/markets.schema';
 import { Product, ProductDocument } from '../products/schemas/product.schema';
+import { User, UserDocument } from '../users/schemas/user.schema';
 
 @Injectable()
 export class MarketsRepository {
@@ -12,6 +13,9 @@ export class MarketsRepository {
 
     // 상품 다큐먼트 주입
     @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+
+    // 유저 다큐먼트 주입
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
 
   // 래포지토리 함수
