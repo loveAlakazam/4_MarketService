88606aa043c9982016f48cd70a2bdb07181bd6e4
eunkangchoi <ekk12mv2@gmail.com>
2022-11-13 21:23:58 +0900

    feat: 상품 데이터베이스단 로직 추가 (상품 등록/수정/삭제)

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -3,21 +3,53 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Product, ProductDocument } from './schemas/product.schema';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
+import { Market, MarketDocument } from '@src/markets/schemas/markets.schema';
+import { User, UserDocument } from '@src/users/schemas/user.schema';
 
 @Injectable()
 export class ProductsRepository {
   constructor(
     // 상품 다큐먼트 주입
-    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>,
   ) {}
 
-  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
-    const newProduct = new this.productModel(createProductDto);
-    return newProduct.save();
+  async createProduct(user: UserDocument, createProductDto: CreateProductDto) {
+    const newProduct = await this.productModel.create({
+      ...createProductDto,
+      user: user._id,
+    });
+
+    return newProduct;
   }
 
-  async findAllProducts(): Promise<Product[]> {
-    const productList = await this.productModel.find().exec();
+  async deleteProduct(productId: string) {
+    await this.productModel.updateOne(
+      { _id: productId },
+      { deletedAt: Date.now() },
+    );
+  }
+
+  async findProductById(productId: string) {
+    return await this.productModel.findOne({ _id: productId, deletedAt: null });
+  }
+
+  async findSellerIdByProductId(productId: string) {
+    const sellerId = await this.productModel.findById(productId);
+    return sellerId?.user;
+  }
+
+  async updateProduct(productId: string, updateProductDto) {
+    await this.productModel.updateOne(
+      { _id: productId },
+      { ...updateProductDto },
+    );
+  }
+
+  async findAllProducts() {
+    const productList = await this.productModel
+      .find({ deletedAt: null })
+      .exec();
     return productList;
   }
 }
