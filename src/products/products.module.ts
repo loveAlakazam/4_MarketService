5407c4f9c06f261f64873a559568f8d5f2dde627
eunkangchoi <ekk12mv2@gmail.com>
2022-11-13 18:10:24 +0900

    fix: 상품 모듈 수정(몽구스 모듈, 유저래포지토리 추가)

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,23 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { ProductsController } from './products.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Product, ProductSchema } from './schemas/product.schema';
-import { ProductsRepository } from './products.repository';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from '../commons/filters/http-exception/http-exception.filter';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { ProductsRepository } from './products.repository';
 import { MarketsRepository } from '../markets/markets.repository';
+import { Product, ProductSchema } from './schemas/product.schema';
+import { Market, MarketSchema } from '../markets/schemas/markets.schema';
+import { User, UserSchema } from '../users/schemas/user.schema';
+import { UsersRepository } from '@src/users/users.repository';
 
 @Module({
   imports: [
-    // 상품 몽구스 모듈 주입
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    // 몽구스 모듈 주입
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: Market.name, schema: MarketSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   controllers: [ProductsController],
   providers: [
     ProductsService,
     ProductsRepository,
     MarketsRepository,
+    UsersRepository,
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
   ],
 })
