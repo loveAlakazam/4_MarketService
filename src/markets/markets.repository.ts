c91d33659540cc3efda03a75c32ae8c22e7fa3b0
eunkangchoi <ekk12mv2@gmail.com>
2022-11-14 02:23:22 +0900

    fix: 요청 유저 타입 변경(User->AccessUser)

--- src/markets/markets.repository.ts
+++ src/markets/markets.repository.ts
@@ -4,6 +4,7 @@ import { Model } from 'mongoose';
 import { Market, MarketDocument } from './schemas/markets.schema';
 import { Product, ProductDocument } from '../products/schemas/product.schema';
 import { User } from '../users/schemas/user.schema';
+import { AccessUser } from 'src/auth/dto/access-user.dto';
 
 @Injectable()
 export class MarketsRepository {
@@ -13,20 +14,19 @@ export class MarketsRepository {
     private readonly marketModel: Model<MarketDocument>,
   ) {}
 
-  async createMarketData(seller: User, product: Product) {
+  async createMarketData(seller: AccessUser, product: Product) {
     const newMarketData = await new this.marketModel({
-      seller: seller,
+      seller: seller._id,
       product: product,
     });
 
     await newMarketData.save();
-    return newMarketData;
   }
 
-  async deleteMarketData(seller: User, productId: string) {
+  async deleteMarketData(seller: AccessUser, productId: string) {
     await this.marketModel
       .updateOne(
-        { product: productId, seller: seller },
+        { product: productId, seller: seller._id },
         { deletedAt: Date.now() },
       )
       .exec();
