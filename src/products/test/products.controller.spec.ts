7e774bc3f471db443ec1c94f05cf133019dfc7f5
eunkangchoi <ekk12mv2@gmail.com>
2022-11-14 06:08:22 +0900

    test: product.controller 환경세팅

--- src/products/test/products.controller.spec.ts
+++ src/products/test/products.controller.spec.ts
@@ -1,17 +1,42 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { MarketsRepository } from '../../markets/markets.repository';
 import { ProductsController } from '../products.controller';
 import { ProductsRepository } from '../products.repository';
 import { ProductsService } from '../products.service';
 import { Product } from '../schemas/product.schema';
 import { Market } from '../../markets/schemas/markets.schema';
-import { User } from '../../users/schemas/user.schema';
+import { SellerGuard } from '../../auth/guards/local-auth.guard';
 
 describe('ProductsController', () => {
+  let app: INestApplication;
   let controller: ProductsController;
   let service: ProductsService;
-  let repository: ProductsRepository;
+  let productRepository: ProductsRepository;
+  let marketRepository: MarketsRepository;
+
+  const mockRepository = {
+    find: jest.fn(),
+    findById: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+    exec: jest.fn(() => true),
+    updateOne: jest.fn(),
+    create: jest.fn(),
+  };
+
+  const mockGuards = {
+    SellerGuard: jest.fn((request) => {
+      const user = request?.session?.passport?.user;
+      if (user) {
+        return user?.isSeller;
+      }
+      return false;
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,11 +56,26 @@ describe('ProductsController', () => {
           useFactory: () => {},
         },
       ],
-    }).compile();
+    })
+      .overrideGuard(SellerGuard)
+      .useValue(mockGuards.SellerGuard)
+      .compile();
+
+    app = module.createNestApplication();
+    app.setGlobalPrefix('/api');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+      }),
+    );
+
+    app.init();
 
     controller = module.get<ProductsController>(ProductsController);
     service = module.get<ProductsService>(ProductsService);
-    repository = module.get<ProductsRepository>(ProductsRepository);
+    productRepository = module.get<ProductsRepository>(ProductsRepository);
+    marketRepository = module.get<MarketsRepository>(MarketsRepository);
   });
 
   it('should be defined', () => {
