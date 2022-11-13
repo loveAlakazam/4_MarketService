09cd3ab9b2505bd8fff3fdcacd8a0840590207f2
eunkangchoi <ekk12mv2@gmail.com>
2022-11-13 21:22:10 +0900

    feat: 상품 등록/삭제 시 마켓에 등록/삭제 로직 추가

--- src/markets/markets.repository.ts
+++ src/markets/markets.repository.ts
@@ -2,21 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Market, MarketDocument } from './schemas/markets.schema';
-import { Product, ProductDocument } from '../products/schemas/product.schema';
-import { User, UserDocument } from '../users/schemas/user.schema';
+import { ProductDocument } from '../products/schemas/product.schema';
+import { UserDocument } from '../users/schemas/user.schema';
 
 @Injectable()
 export class MarketsRepository {
   constructor(
     // 마켓 다큐먼트 주입
-    @InjectModel(Market.name) private marketModel: Model<MarketDocument>,
-
-    // 상품 다큐먼트 주입
-    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
-
-    // 유저 다큐먼트 주입
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Market.name)
+    private readonly marketModel: Model<MarketDocument>,
   ) {}
 
-  // 래포지토리 함수
+  async createMarketData(seller: UserDocument, product: ProductDocument) {
+    const newMarketData = await new this.marketModel({
+      seller: seller._id,
+      product: product._id,
+    });
+
+    await newMarketData.save();
+    return newMarketData;
+  }
+
+  async deleteMarketData(seller: UserDocument, productId: string) {
+    await this.marketModel
+      .updateOne(
+        { product: productId, seller: seller._id },
+        { deletedAt: Date.now() },
+      )
+      .exec();
+  }
 }
