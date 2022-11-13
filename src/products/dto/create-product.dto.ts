d301d2cbbef1fbad3499721dbcc080222b613514
eunkangchoi <ekk12mv2@gmail.com>
2022-11-13 16:09:34 +0900

    fix: create-product.dto 수정

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,24 @@
-export class CreateProductDto {}
+import { IsString, IsNumber, IsEnum } from 'class-validator';
+import { PRODUCT_CATEGORIES } from '../enums/categories';
+import { PRODUCT_COUNTRIES } from '../enums/countries';
+
+export class CreateProductDto {
+  @IsString()
+  name: string; //상품명
+
+  @IsString()
+  @IsEnum(PRODUCT_CATEGORIES)
+  category: string; // 상품카테고리
+
+  @IsString()
+  @IsEnum(PRODUCT_COUNTRIES)
+  buyCountry: string; //구매국가
+
+  @IsNumber()
+  price: number; // 상품가격
+
+  @IsString()
+  description: string; //상품요약설명
+
+  closeDate?: Date; // 주문마감일
+}
