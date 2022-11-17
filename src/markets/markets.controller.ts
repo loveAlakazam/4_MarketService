c479ecef33c0b55cde72198c97f32bee21d9759c
eunkangchoi <ekk12mv2@gmail.com>
2022-11-17 23:54:14 +0900

    :art: feat: 마켓조회 라우터 추가(+불필요 라우터 제거)

--- src/markets/markets.controller.ts
+++ src/markets/markets.controller.ts
@@ -1,23 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  HttpStatus,
-  UseFilters,
-  Inject,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Controller, Get, UseFilters, Query } from '@nestjs/common';
 import { MarketsService } from './markets.service';
-import { CreateMarketDto } from './dto/create-market.dto';
-import { UpdateMarketDto } from './dto/update-market.dto';
-import { MarketCustomException } from './market-exception';
 import { HttpExceptionFilter } from '../commons/filters/http-exception/http-exception.filter';
-
-import { Logger, LoggerService } from '@nestjs/common';
+import { SearchMarket } from './dto/search-market.dto';
 
 // HttpExceptionFilter 을 컨트롤러에서 적용.
 @UseFilters(new HttpExceptionFilter())
@@ -25,28 +9,8 @@ import { Logger, LoggerService } from '@nestjs/common';
 export class MarketsController {
   constructor(private readonly marketsService: MarketsService) {}
 
-  @Post()
-  create(@Body() createMarketDto: CreateMarketDto) {
-    return;
-  }
-
   @Get()
-  findAll() {
-    return this.marketsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.marketsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMarketDto: UpdateMarketDto) {
-    return this.marketsService.update(+id, updateMarketDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.marketsService.remove(+id);
+  async searchMarket(@Query() search: SearchMarket) {
+    return await this.marketsService.searchMarket(search);
   }
 }
