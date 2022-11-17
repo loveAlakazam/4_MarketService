166a1af6a4c77ad0c387f901cf8a666e26fdfb15
eunkangchoi <ekk12mv2@gmail.com>
2022-11-17 23:59:02 +0900

    :sparkles: feat: 상품 조회 & 검색 api 추가

--- src/markets/markets.repository.ts
+++ src/markets/markets.repository.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Market, MarketDocument } from './schemas/markets.schema';
-import { Product, ProductDocument } from '../products/schemas/product.schema';
-import { User } from '../users/schemas/user.schema';
-import { AccessUser } from 'src/auth/dto/access-user.dto';
+import { Product } from '../products/schemas/product.schema';
+import { AccessUser } from '../auth/dto/access-user.dto';
+import { SearchMarket } from './dto/search-market.dto';
+import { PER_PAGE } from './dto/market-pagination';
 
 @Injectable()
 export class MarketsRepository {
@@ -29,6 +30,95 @@ export class MarketsRepository {
         { product: productId, seller: seller._id },
         { deletedAt: Date.now() },
       )
+      .exec();
+  }
+
+  async searchMarket(search: SearchMarket) {
+    // (선택) 상품명(search.name) 가 null 이 아니라면 검색 쿼리 추가
+    let searchNameQuery = null;
+    if (search.name) {
+      searchNameQuery = {
+        'product.name': { $regex: '.*' + search.name + '.*' },
+      };
+    }
+
+    // (선택) 상품 카테고리(search.category)가 null이 아니라면 검색 쿼리 추가
+    let searchCategoryQuery = null;
+    if (search.category) {
+      searchCategoryQuery = {
+        'product.category': { $in: [search.category] },
+      };
+    }
+
+    // (선택) 상품 구매국가(search.buyCountry)가 null 이 아니라면 검색조건 추가
+    let searchBuyCountryQuery = null;
+    if (search.buyCountry) {
+      searchBuyCountryQuery = {
+        'product.buyCountry': { $in: [search.buyCountry] },
+      };
+    }
+
+    const query = this.marketModel.aggregate([
+      {
+        $lookup: {
+          from: 'users', // user 콜렉션과 join
+          localField: 'seller',
+          foreignField: '_id',
+          as: 'seller',
+        },
+      },
+      {
+        $lookup: {
+          from: 'products', // product 데이터과 join
+          localField: 'product',
+          foreignField: '_id',
+          as: 'product',
+        },
+      },
+      {
+        $project: {
+          product: 1, // product 데이터를 갖는다.
+          seller: {
+            // 판매자의 _id, name, sellerNickname 컬럼만 추출한다. (일부컬럼 추출)
+            _id: 1,
+            name: 1,
+            sellerNickname: 1,
+          },
+        },
+      },
+      {
+        $match: {
+          // 상품은 삭제되지 않은 상태 (product.deletedAt !== null)
+          'product.deletedAt': null,
+        },
+      },
+      {
+        $match: {
+          // 상품명 검색 쿼리
+          ...searchNameQuery,
+
+          // 상품 카테고리 검색 쿼리
+          ...searchCategoryQuery,
+
+          // 상품 구매국가 검색쿼리
+          ...searchBuyCountryQuery,
+        },
+      },
+    ]);
+
+    // 페이지네이션 추가
+    query
+      .limit(PER_PAGE) // 한 페이지당 20개
+      .skip(PER_PAGE * (search.page - 1));
+
+    // 날짜 sort 옵션 추가
+    let sortCloseDateQuery = null;
+    if (search.closeDate) {
+      sortCloseDateQuery = { 'product.closeDate': 1 };
+    }
+
+    return await query
+      .sort({ 'product.createdAt': -1, ...sortCloseDateQuery })
       .exec();
   }
 }
