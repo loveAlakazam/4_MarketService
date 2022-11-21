e58b44ad8babfa589803ca3bbc10fc6c4e6c3c82
loveAlakazam <dmsrkd1216@gmail.com>
2022-11-21 22:41:50 +0900

    :label: update: product-seller-info 에 예금주/은행명/계좌번호 필드 추가

--- src/products/dto/product-detail-info.dto.ts
+++ src/products/dto/product-detail-info.dto.ts
@@ -8,6 +8,9 @@ export class ProductSellerInfo {
   phoneNumber: string;
   isSeller: boolean;
   sellerNickname: string;
+  accountName?: string; // 예금주명
+  accountBank?: string; // 은행
+  accountNumber?: string; // 계좌번호
 }
 
 // 상품정보
