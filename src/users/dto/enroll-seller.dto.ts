f6fe07e1af29044a054ddfcaa71063103ef68419
loveAlakazam <dmsrkd1216@gmail.com>
2022-11-21 22:40:55 +0900

    :label: update: enroll-seller-dto 에 예금주/은행명/계좌번호 필드추가

--- src/users/dto/enroll-seller.dto.ts
+++ src/users/dto/enroll-seller.dto.ts
@@ -1,6 +1,21 @@
-import { IsString } from 'class-validator';
+import { IsEnum, IsString, Matches, MinLength } from 'class-validator';
+import { ACCOUNT_NUMBER_REGEX } from '../../commons/regexps/regexp';
+import { BANK_NAMES } from '../enums/accountBankEnum';
 
 export class EnrollSellerDto {
   @IsString()
-  sellerNickname: string;
+  @MinLength(2)
+  sellerNickname: string; // 셀러닉네임
+
+  @IsString()
+  @MinLength(2)
+  accountName: string; // 예금주
+
+  @IsString()
+  @IsEnum(BANK_NAMES)
+  accountBank: string; // 은행명
+
+  @IsString()
+  @Matches(ACCOUNT_NUMBER_REGEX)
+  accountNumber: string; // 계좌번호
 }
